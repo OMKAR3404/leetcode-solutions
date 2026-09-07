e343de6982e3e73f63e95d4d44cb7b11c8393209
Omkar more <146331673+OMKAR3404@users.noreply.github.com>
2026-09-08 01:10:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (24.62%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.c
@@ -0,0 +1,31 @@
+
+int min(int x,int y){
+    if(x<y){
+        return x;
+    }else if (x==y){
+        return x;
+    }
+    else{
+        return y;
+    }
+}
+
+
+
+int minCostClimbingStairs(int* cost, int costSize) {
+    int n = costSize;
+    int dp[n];
+
+    dp[0] = cost[0];
+    dp[1] = cost[1];
+
+    for(int i=2;i<n;i++){
+        dp[i] = cost[i] + min(dp[i-1],dp[i-2]);
+    }
+
+    int answer = min(dp[n-1],dp[n-2]);
+
+    return answer ;
+
+    
+}
